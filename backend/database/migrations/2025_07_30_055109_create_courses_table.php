5213d77d2205625fd3b36c189b780236488b2393
nayan97 <nayanislam085@gmail.com>
2025-07-30 12:35:59 +0600

    created category level courses model migration

---
+++ backend/database/migrations/2025_07_30_055109_create_courses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+      
+
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Assuming relation to users table
+            $table->foreignId('category_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('level_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('language_id')->nullable()->constrained()->onDelete('cascade');
+            $table->text('description')->nullable();
+            $table->decimal('price', 8, 2)->default(0.00);
+            $table->decimal('cross_price', 8, 2)->nullable();
+            $table->integer('status')->default(0); // e.g. 1 = active, 0 = inactive
+            $table->enum('is_featured', ['yes', 'no'])->default('no');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
