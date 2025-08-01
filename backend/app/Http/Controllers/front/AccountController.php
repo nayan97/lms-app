7798da81714be7b359d38efc36198168b9a163e8
nayan97 <nayanislam085@gmail.com>
2025-08-01 11:15:16 +0600

    login api created

--- backend/app/Http/Controllers/front/AccountController.php
+++ backend/app/Http/Controllers/front/AccountController.php
@@ -16,7 +16,7 @@ class AccountController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|min:5',
             'email' => 'required|email|unique:users,email',
-            'password' => 'required|string|min:6', // expects password_confirmation
+            'password' => 'required|string|min:6',
         ]);
 
         // Handle validation failure
@@ -41,5 +41,37 @@ class AccountController extends Controller
             'message' => 'User registered successfully',
             'user' => $user
         ],200);
+    }
+
+
+    // auth function
+
+    public function authenticate(Request $request) {
+        $credentials = $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    // Attempt to find user
+    $user = User::where('email', $credentials['email'])->first();
+
+    if (!$user || !Hash::check($credentials['password'], $user->password)) {
+        return response()->json([
+            'status' => false,
+            'message' => 'Invalid email or password.'
+        ], 401);
+    }
+
+    // Create token using Sanctum
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    return response()->json([
+        'status' => 200,
+        'message' => 'Login successful.',
+        'name' => $user->name,
+        'id' => $user->id,
+        'token' => $token,
+    ], 401);
+        
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\front\AccountController;
 
 Route::post('/register', [AccountController::class, 'register']);
+Route::post('/login', [AccountController::class, 'authenticate']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
